46e4d4cae2aa89f048ae54dd4868662dd967bfd4
NookieGrey <gaguliyanugzar@gmail.com>
2018-11-18 18:07:31 +0500

    split js into chunks

---
+++ README.md
@@ -0,0 +1,8 @@
+#Front-end profession
+
+Presentation on GDG Tashkent 2018
+
+Simple example of HTML+CSS+JS app,  
+that's use google search open API  
+for search on facebook website any information  
+using native JavaScript
--- index.js
+++ index.js
@@ -2,33 +2,41 @@ const form = document.getElementById("form");
 const template = document.getElementById("result-template").querySelector(".result-row");
 const resultContainer = document.getElementById("results");
 
-const API_KEY = "AIzaSyD21zAUiEOASgPFg1WcQRqmrEJ3lkT4tl0";
-const SEARCH_ENGINE_ID = "008352208444969352903:hueghfwucug";
+form.addEventListener("submit", onSubmit);
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+function onSubmit(event) {
+  event.preventDefault();
 
-    const searchQuery = event.target.elements.query.value || "GDG";
+  const searchQuery = event.target.elements.query.value || "GDG";
 
-    fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${SEARCH_ENGINE_ID}&q=${searchQuery}`)
-        .then(response => response.json())
-        .then(response => {
-            resultContainer.innerHTML = "";
+  makeRequest(searchQuery);
+}
 
-            response.items.forEach(item => {
-               const row = template.cloneNode(true);
+function makeRequest(searchQuery) {
+  const API_KEY = "AIzaSyD21zAUiEOASgPFg1WcQRqmrEJ3lkT4tl0";
+  const ENGINE_ID = "008352208444969352903:hueghfwucug";
 
-               const title = row.querySelector(".result-title");
-               title.innerHTML = item.htmlTitle;
-               title.href = item.link;
+  fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${ENGINE_ID}&q=${searchQuery}`)
+    .then(response => response.json())
+    .then(response => {
+      resultContainer.innerHTML = "";
 
-               const url = row.querySelector(".result-url");
-               url.innerHTML = item.htmlFormattedUrl;
+      response.items.forEach(insertRow);
+    });
+}
 
-               const description = row.querySelector(".result-description");
-               description.innerHTML = item.htmlSnippet;
+function insertRow(item) {
+  const row = template.cloneNode(true);
 
-               resultContainer.appendChild(row);
-            });
-        });
-});
+  const title = row.querySelector(".result-title");
+  title.innerHTML = item.htmlTitle;
+  title.href = item.link;
+
+  const url = row.querySelector(".result-url");
+  url.innerHTML = item.htmlFormattedUrl;
+
+  const description = row.querySelector(".result-description");
+  description.innerHTML = item.htmlSnippet;
+
+  resultContainer.appendChild(row);
+}
